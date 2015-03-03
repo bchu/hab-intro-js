8167698ecb8f9cd6fdefcba806b9c02d43215696
Brian Chu <bc@brianchu.com>
2015-03-03 00:45:38 -0800

    Fix python exercises.py

--- exercises.py
+++ exercises.py
@@ -1,13 +1,19 @@
+# WARNING: THIS CODE IS ONLY INTENDED AS A GUIDELINE
+# FOR THE KIND OF LOGIC NEEDED TO IMPLEMENT exercises.js
+# invoke_once and enforce_argument_count don't actually work.
+
 def map(list, mapper):
   return [mapper(item) for item in list]
 
 def reduce(list, reducer, initializer=None):
   accum_value = initializer
-  for x in it:
-      accum_value = function(accum_value, x)
+  for x in list:
+      accum_value = reducer(accum_value, x)
   return accum_value
 
-def invokeOnce(func):
+# THIS DOES NOT ACTUALLY WORK IN PYTHON
+# IT'S ONLY A GUIDELINE FOR THE JAVASCRIPT VERSION
+def invoke_once(func):
   called = False
   def wrapper():
     nonlocal called
@@ -17,6 +23,8 @@ def invokeOnce(func):
     func()
   return wrapper
 
+# THIS DOES NOT ACTUALLY WORK IN PYTHON
+# IT'S ONLY A GUIDELINE FOR THE JAVASCRIPT VERSION
 def enforce_argument_count(func, default_value, argument_count):
   def wrapper(*args):
     if len(args) < argument_count:
